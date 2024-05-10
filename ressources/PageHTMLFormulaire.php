b4967b93a2fd7bd26128904bfd71fd1584ef6390
alassanelayediop <diopalassanelaye3@gmamil.com>
2024-05-10 11:40:02 +0000

    Creation de 2 nouveaux dossiers (css et images) dans le dossierpublic

--- ressources/PageHTMLFormulaire.php
+++ ressources/PageHTMLFormulaire.php
@@ -4,7 +4,7 @@
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
-        <link rel="stylesheet" href="../public/PageCSS.css">
+        <link rel="stylesheet" href="../public/css/PageCSS.css">
         <title>SOLOQUIZZ</title>
     </head>
     <body>
@@ -12,7 +12,7 @@
             <nav class="px-3 navbar navbar-expand-lg navbar-dark bg-dark fixed">
                 <div class="container row px-1 containerClass">
                     <div class="col">
-                        <a class="navbar-brand" href="#"><img class="imgClass1" src="Soloquizz.png"></a>
+                        <a class="navbar-brand" href="#"><img class="imgClass1" src="../public/images/Soloquizz.png"></a>
                     </div>
                     <div class="col divClass1">
                         <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDarkDropdown" aria-controls="navbarNavDarkDropdown" aria-expanded="false" aria-label="Toggle navigation">
@@ -22,7 +22,7 @@
                             <ul class="navbar-nav">
                                 <li class="nav-item dropdown">
                                     <button class="px-3 btn btn-dark dropdown-toggle" data-bs-toggle="dropdown" aria-expanded="false">
-                                        <img class="imgClass2" src="user.png">
+                                        <img class="imgClass2" src="../public/images/user.png">
                                     </button>
                                     <ul class="dropdown-menu dropdown-menu-info">
                                         <li>
