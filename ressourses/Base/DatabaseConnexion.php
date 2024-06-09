f446cd0bd8b69c340d3f93d72a6b811528e2173d
SALOMON0003 <139631313+SALOMON0003@users.noreply.github.com>
2024-06-09 02:07:10 +0000

    Correction des erreurs

--- ressourses/Base/DatabaseConnexion.php
+++ ressourses/Base/DatabaseConnexion.php
@@ -7,14 +7,11 @@
 class DatabaseConnexion
 {
     private $host = 'localhost';
-    private $dbname = 'testDB'; // Modifiez le Nom de la base de données
+    private $dbname = 'testDB'; // Modifie le Nom de la base de données
     private $username = 'SALOMON'; // Modifie le Nom d'utilisateur pour la connexion à la base de données
-    private $password = 'S@lomon1501'; // Modifiez le Mot de passe pour la connexion à la base de données
+    private $password = 'S@lomon1501'; // Modifie le Mot de passe pour la connexion à la base de données
     private $conn;
-    /**
-     * Établit une connexion à la base de données.
-     * @return PDO|null
-     */
+    
     public function __construct()
     {
         $this->conn = null;
@@ -24,12 +21,7 @@ class DatabaseConnexion
             echo "Erreur de connexion: " . $e->getMessage();
         }
     }
-    /**
-     * Exécute une requête SQL avec des paramètres facultatifs.
-     * @param string $query La requête SQL à exécuter.
-     * @param array $params Les paramètres à lier à la requête.
-     * @return PDOStatement|null
-     */
+    
     public function executeQuery($query, $params = [])
     {
         try {
